823a7f75beb645c73d45f40cdee7f3a0c66f41be
ShreyaCodes-It <shreyaasingh0912@gmail.com>
2026-04-13 10:50:30 +0530

    Add files via upload

---
+++ day9.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverse(char str[], int start, int end) {
+    if (start >= end)
+        return;
+
+    // swap characters
+    char temp = str[start];
+    str[start] = str[end];
+    str[end] = temp;
+
+    // recursive call
+    reverse(str, start + 1, end - 1);
+}
+
+int main() {
+    char str[100];
+    scanf("%s", str);
+
+    int n = strlen(str);
+
+    reverse(str, 0, n - 1);
+
+    printf("%s", str);
+
+    return 0;
+}
