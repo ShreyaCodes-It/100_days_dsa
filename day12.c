c1734075843600000daeca5ea74f56b5c816f1bb
ShreyaCodes-It <shreyaasingh0912@gmail.com>
2026-04-16 14:45:37 +0530

    Update day12.c

--- day12.c
+++ day12.c
@@ -2,27 +2,28 @@
 
 int main() {
     int m, n;
+
     scanf("%d %d", &m, &n);
 
-    int A[m][n];
+    int a[m][n];
 
-    // Input matrix
+    // input matrix
     for(int i = 0; i < m; i++) {
         for(int j = 0; j < n; j++) {
-            scanf("%d", &A[i][j]);
+            scanf("%d", &a[i][j]);
         }
     }
 
-    // Check if square matrix
+    // check square matrix first
     if(m != n) {
         printf("Not a Symmetric Matrix");
         return 0;
     }
 
-    // Check symmetry
+    // check symmetry
     for(int i = 0; i < m; i++) {
         for(int j = 0; j < n; j++) {
-            if(A[i][j] != A[j][i]) {
+            if(a[i][j] != a[j][i]) {
                 printf("Not a Symmetric Matrix");
                 return 0;
             }
