ff8581514670edf3a65582c2298984814c3aa8bb
ShreyaCodes-It <shreyaasingh0912@gmail.com>
2026-04-20 21:44:47 +0530

    Add files via upload

---
+++ lab21.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Node *head = NULL, *temp = NULL, *newnode = NULL;
+
+    for(int i = 0; i < n; i++) {
+        newnode = (struct Node*)malloc(sizeof(struct Node));
+        scanf("%d", &newnode->data);
+        newnode->next = NULL;
+
+        if(head == NULL) {
+            head = temp = newnode;
+        } else {
+            temp->next = newnode;
+            temp = newnode;
+        }
+    }
+
+    temp = head;
+
+    while(temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
