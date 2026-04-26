61859651aa1e09d7342b58c21ac9dda126f47a1a
ShreyaCodes-It <shreyaasingh0912@gmail.com>
2026-04-26 17:11:24 +0530

    Add files via upload

---
+++ day36.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+#define MAX 1000
+
+int queue[MAX];
+int front = -1, rear = -1;
+
+void enqueue(int x) {
+    if ((rear + 1) % MAX == front) return;
+
+    if (front == -1) front = 0;
+
+    rear = (rear + 1) % MAX;
+    queue[rear] = x;
+}
+
+int dequeue() {
+    if (front == -1) return -1;
+
+    int val = queue[front];
+
+    if (front == rear) {
+        front = rear = -1;
+    } else {
+        front = (front + 1) % MAX;
+    }
+
+    return val;
+}
+
+void display() {
+    if (front == -1) return;
+
+    int i = front;
+    while (1) {
+        printf("%d ", queue[i]);
+        if (i == rear) break;
+        i = (i + 1) % MAX;
+    }
+}
+
+int main() {
+    int n, m, x;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        enqueue(x);
+    }
+
+    scanf("%d", &m);
+
+    for (int i = 0; i < m; i++) {
+        int val = dequeue();
+        enqueue(val);  // 🔥 rotation step
+    }
+
+    display();
+
+    return 0;
+}
